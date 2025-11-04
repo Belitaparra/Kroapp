204dc0210326cab0d2e480836b68e4555038adcb
Belitaparra <ilombanaparra24@gmail.com>
2025-11-04 00:52:33 -0500

    Add files via upload
    añadí el paquete del proyecto

---
+++ kroagapp_1/bin/kroagapp_1.dart
@@ -0,0 +1,7 @@
+import 'package:kroagapp_1/kroagapp_1.dart' as kroagapp_1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${kroagapp_1.calculate()}!');
+
+  print("hi");
+}
---
+++ kroagapp_1/test/kroagapp_1_test.dart
@@ -0,0 +1,8 @@
+import 'package:kroagapp_1/kroagapp_1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
